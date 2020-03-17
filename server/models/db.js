2b469f0c0cff7b8a46039c254c284b147502400b
Ami des jeunes <habajeunes2@gmail.com>
2020-03-17 13:47:01 +0200

    travis now

--- server/models/db.js
+++ server/models/db.js
@@ -2,8 +2,9 @@ import { Pool } from 'pg';
 import config from './dbConfig';
 
 // const connString = "postgres://proccess.env.DB_USER:proccess.env.PASSWORD@proccess.env.DB_HOST:5432/proccess.env.DB"
+const connString =process.env.DATABASE_URL || config
 
-export const pool = new Pool(config)
+export const pool = new Pool(connString)
 
  pool.on('connect', () => {
     console.log('connected to the database successful!');
